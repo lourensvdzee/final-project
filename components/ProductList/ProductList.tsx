c317d0824f7c7ed14ece927f1b3e7367d1de4e37
Lourens-Spiced <lourensvdzee@gmail.com>
2023-05-19 17:05:01 +0200

    Product List styling finished so far

--- components/ProductList/ProductList.tsx
+++ components/ProductList/ProductList.tsx
@@ -6,14 +6,17 @@ import { DbProduct } from "../../db/models/DbProduct";
 import {
   CardList,
   Card,
+  ProductName,
+  ProductInfo,
   ImageWrapper,
   Image,
-  ProductInfoWrapper,
-  ProductInfo,
-  ProductName,
+  EanWrapper,
   EAN,
-  Price,
-  Durability,
+  ProductInfoRight,
+  PriceTitle,
+  PriceValue,
+  DurabilityTitle,
+  DurabilityValue,
 } from "./ProductListStyles";
 
 interface Props {
@@ -27,24 +30,24 @@ const ProductList: React.FC<Props> = ({ products }) => {
         {products.map((product) => (
           <Card key={product._id}>
             <ProductName>
-              {product.title.length > 24
-                ? product.title.slice(0, 24) + "..."
+              {product.title.length > 37
+                ? product.title.slice(0, 37) + "..."
                 : product.title}
             </ProductName>
-            <br />
-            <ProductInfoWrapper>
+            <ProductInfo>
               <ImageWrapper>
                 <Image src={product.images[0]} alt="Product Image" />
               </ImageWrapper>
-              <ProductInfo>
+              <EanWrapper>
                 <EAN>EAN: {product.ean}</EAN>
-                <Durability>Durability:</Durability>
-                <Price>
-                  Price:
-                  <br />${product.lowest_recorded_price}
-                </Price>
-              </ProductInfo>
-            </ProductInfoWrapper>
+              </EanWrapper>
+              <ProductInfoRight>
+                <PriceTitle>Price:</PriceTitle>
+                <PriceValue>${product.lowest_recorded_price}</PriceValue>
+                <DurabilityTitle>Durability:</DurabilityTitle>
+                <DurabilityValue>2-3 years</DurabilityValue>
+              </ProductInfoRight>
+            </ProductInfo>
           </Card>
         ))}
       </CardList>
--- components/ProductList/ProductListStyles.js
+++ components/ProductList/ProductListStyles.js
@@ -4,6 +4,7 @@
 import styled from 'styled-components';
 
 export const CardList = styled.ul`
+  list-style: none;
   color: #6f6f6f;
   display: flex;
   justify-content: center;
@@ -14,29 +15,32 @@ export const CardList = styled.ul`
 `;
 
 export const Card = styled.li`
-  min-width: 230px;
+  width: 320px;
   min-height: 200px;
   padding: 10px;
   border-radius: 10px;
-  display: flex;
   box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.4);
-`;
+  `;
 
-export const ProductInfoWrapper = styled.div`
+export const ProductName = styled.h3`
+  margin: 0;
+  white-space: nowrap;
+  `;
+
+export const ProductInfo = styled.div`
   display: grid;
-  grid-template-columns: auto 1fr;
-  grid-template-rows: auto auto;
-  grid-column-gap: 10px;
-  grid-row-gap: 10px;
-  align-items: center;
+  grid-template-columns: 1fr 1fr;
+  /* grid-template-columns: repeat(4, 1fr); */
 `;
 
-export const ImageWrapper = styled.div`   
-  grid-row: 1 / 3;
-  max-width: 100%;
-  max-height: 100%;
-  /* display: flex;
-  flex-wrap: wrap; */
+export const ImageWrapper = styled.div`
+  max-width:150px;
+  max-height: 150px;
+  grid-column: 1 / span 2;
+  grid-row: 1; 
+  margin-top: 10px;
+  display: flex;
+  justify-content: center;
   align-items: center;
   border-radius: 10px;
   background-color: #fff;
@@ -45,82 +49,63 @@ export const ImageWrapper = styled.div`
 `;
 
 export const Image = styled.img`
-  display: inline-block;
-  max-width: 30vw;
-  max-height:42vw;
-  object-fit: cover;
-  margin: 5px;
-
-/* @media (min-width: 500px) {
-    max-width: 20vw;
-    max-height:25vw;
-}
-
-@media (min-width: 800px) {
-    max-width: 15vw;
-    max-height: 20vw;
-}
-
-@media (min-width: 1000px) {
-    max-width: 10vw;
-    max-height: 15vw;
-}
-
-@media (min-width: 1600px) {
-    max-width: 5vw;
-    max-height: 10vw;
-} */
+  max-width: 95%;
+  max-height: 95%;
 `;
 
-// export const ProductInfoWrapper = styled.div`
-//   display: flex;
-//   flex-direction: column;
-//   margin: 0px 0px 0px 5px;
-//   `;
-
-export const ProductInfo = styled.div`
-  grid-column: 2;
-  padding: 10px;
-  border-radius: 10px;
+export const EanWrapper = styled.div`
+  max-width:150px;
+  max-height: 150px;
+  grid-column: 1;
+  grid-row: 2;
   background-color: #fff;
+  border-radius: 10px;
   border: 1px solid #ccc;
   box-shadow: 0 0 10px 0 #ccc;
-  /* flex-grow: 1; */
-  /* display: flex; */
-  /* flex-direction: column; */
-`;
-
-export const ProductName = styled.h3`
-  margin: 0;
-`;
-
-export const Price = styled.p`
-  font-size: 12px;
-`;
+  margin-top: 5px;
+  `;
 
 export const EAN = styled.p`
-  margin: 0;
-  font-size: 12px;
+  margin: 5px;
+  text-align: center;
 `;
 
-// export const Description = styled.p`
-//   margin: 5px 0;
-//   overflow: hidden;
-//   text-overflow: ellipsis;
-// `;
-
-// export const MoreLink = styled.a`
-//   color: blue;
-//   cursor: pointer;
-// `;
-
-export const Durability = styled.p`
-  margin: 5px 0px 0px 0px;
-  font-style: italic;
-  padding: 10px;
+export const ProductInfoRight = styled.div`
+  grid-column: 2;
+  grid-row: 1 / span 2;
+  margin-top: 10px;
+  background-color: #fff;
   border-radius: 10px;
-  background-color: #F8F6F4;
   border: 1px solid #ccc;
   box-shadow: 0 0 10px 0 #ccc;
+  display: flex;
+  flex-direction: column;
+  `;
+
+export const PriceTitle = styled.p`
+justify-self: center;
+align-self: center;
+margin-top: 20px;
 `;
 
+export const PriceValue = styled.p`
+justify-self: center;
+align-self: center;
+font-weight: bold;
+font-size: 1.6em;
+margin-top: 0px;
+`;
+
+export const DurabilityTitle = styled.p`
+justify-self: center;
+align-self: center;
+margin-top: 20px;
+`;
+
+export const DurabilityValue = styled.p`
+justify-self: center;
+align-self: center;
+font-weight: bold;
+font-size: 1.4em;
+margin-top: 0px;
+`;
