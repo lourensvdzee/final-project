aabea552a0e52d5f6ac6bf947e99cda6f7d1b114
Lourens-Spiced <lourensvdzee@gmail.com>
2023-05-25 12:03:38 +0200

    2nd attempt

--- db/models/ApiProduct.tsx
+++ db/models/ApiProduct.tsx
@@ -1,6 +1,55 @@
-//This is where the API product schema will be defined later on.
+/* db/models/ApiProduct.tsx */
+
+/* import fetch from "node-fetch";
 
 export interface ApiProduct {
-  name: string;
-  sku: string;
+  title: string;
+  ean: string;
+  images: string[];
+  lowest_recorded_price: number;
 }
+
+interface ApiItem {
+  title: string;
+  ean: string;
+  images: string[];
+  offers: {
+    price: number;
+  }[];
+}
+
+interface ApiResponse {
+  items: ApiItem[];
+}
+
+// Fetch API products based on search query
+export const fetchApiProducts = async (
+  searchQuery: string
+): Promise<ApiProduct[]> => {
+  const apiUrl = "https://api.upcitemdb.com/prod/trial/search";
+  const matchMode = 1;
+  const type = "product";
+
+  try {
+    const response = await fetch(
+      `${apiUrl}?s=${searchQuery}&match_mode=${matchMode}&type=${type}`
+    );
+    const data = (await response.json()) as ApiResponse;
+
+    if (data && data.items && data.items.length > 0) {
+      const apiProducts = data.items.map((item) => ({
+        title: item.title,
+        ean: item.ean,
+        images: item.images,
+        lowest_recorded_price: item.offers[0]?.price || 0,
+      }));
+
+      return apiProducts;
+    } else {
+      return [];
+    }
+  } catch (error) {
+    console.error("Error fetching API products:", error);
+    throw error;
+  }
+}; */
