0e4308f98833ca575426f166028668e7b171a42a
Lourens-Spiced <lourensvdzee@gmail.com>
2023-05-30 12:40:15 +0200

    init add product

--- pages/database/searchDbProduct/[id].tsx
+++ pages/database/searchDbProduct/[id].tsx
@@ -1,12 +1,17 @@
+/* pages/database/searchDbProduct/[id].tsx
+\ */
+
+//Not sure if this page is actually used
+
 import dbConnect from "../../../db/connect";
-import Product from "../../../db/models/DbProduct";
+import { DbProduct } from "../../../db/models/DbProduct";
 
 export default async function handler(request, response) {
   await dbConnect();
   const { id } = request.query;
 
   if (request.method === "GET") {
-    const product = await Product.findById(id);
+    const product = await DbProduct.findById(id);
 
     if (!product) {
       return response.status(404).json({ status: "Not Found" });
